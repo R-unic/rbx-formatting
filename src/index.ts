27cf34b7626df64b3f017cedbbd0fb95085bd5b0
R-unic <roleyspersonal@gmail.com>
2025-03-21 15:30:27 -0400

    test: everything

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,110 @@
-import { trim } from "@rbxts/string-utils";
-
 const { floor, log } = math;
+
+const trim = (s: string, regex = "%s*") => s.match(`^${regex}(.-)${regex}$`)[0] as string;
+const capitalize = (s: string) => s.sub(1, 1).upper() + s.sub(2);
+
+const caseInsensitiveRegexCache = new Map<string[], string>;
+
+function createCaseInsensitiveRegex(strings: string[]): string {
+  if (caseInsensitiveRegexCache.has(strings))
+    return caseInsensitiveRegexCache.get(strings)!;
+
+  const escapedStrings: string[] = [];
+  for (const s of strings) {
+    const [pattern] = s.gsub("%a", c => "(%s%s)".format(c.upper(), c.lower()));
+    escapedStrings.push(pattern);
+  }
+
+  const fullRegex = escapedStrings.join("|");
+  caseInsensitiveRegexCache.set(strings, fullRegex);
+  return fullRegex;
+}
 
 /**
  * Places commas between every three decimal points in `n`
+ * @param n The number to format
+ * @param minimum If `n` is less than this number, return `n` as a string
+ * @param separator The separator to use (default: ",")
+ * @param decimal The decimal separator to use (default: ".")
  * @example commaFormat(1000000) // 1,000,000
  */
-export function commaFormat(n: number | string): string {
-	let formatted = tostring(n)
-		.reverse()
-		.gsub("(%d%d%d)", "%1,")[0]
-		.reverse();
+export function commaFormat(n: number | string, minimum?: number, separator = ",", decimal = "."): string {
+  const nString = tostring(n);
+  if (minimum !== undefined && tonumber(n)! !== undefined && tonumber(n)! < minimum)
+    return nString;
 
-	if (formatted.sub(1, 1) === ",")
-		formatted = formatted.sub(2);
+  const [integerPart, decimalPart] = nString.match(`([^%.]+)%${decimal}?(.*)`);
+  let formatted = (integerPart as string)
+    .reverse()
+    .gsub("(%d%d%d)", "%1" + separator)[0]
+    .reverse();
 
-	return formatted;
+  if (formatted.sub(1, 1) === separator)
+    formatted = formatted.sub(2);
+
+  if (decimalPart !== undefined && decimalPart !== "")
+    formatted += decimal + decimalPart
+
+  return formatted;
 }
 
-const baseSuffixes = [
-	"K",
-	"M", "B", "T", "Qd", "Qt", "Sx", "Sp", "Oc", "No", "Dc",
-	"Udc", "Ddc", "Tdc", "Qdc", "Qnd", "Sxd", "Spd", "Ocd", "Nvd", "Vg",
-	"Uvg", "Dvg", "Tvg", "Qvg", "Qnv", "Sxv", "Spv", "Ocv", "Nvv", "Tg",
-	"Utg", "G"
+const defaultSuffixes = [
+  "K",
+  "M", "B", "T", "Qd", "Qt", "Sx", "Sp", "Oc", "No", "Dc",
+  "Udc", "Ddc", "Tdc", "Qdc", "Qnd", "Sxd", "Spd", "Ocd", "Nvd", "Vg",
+  "Uvg", "Dvg", "Tvg", "Qvg", "Qnv", "Sxv", "Spv", "Ocv", "Nvv", "Tg",
+  "Utg", "G"
 ];
+createCaseInsensitiveRegex(defaultSuffixes); // cache it so it isnt generated when you call parseAbbreviated()
 
 /**
  * Abbreviates numbers larger than or equal to `threshold`
  * @example
  * abbreviate(1000000) // 1M
- * abbreviate(1000000000000000000000) // 1Sx
+ * abbreviate(1000000000000000000) // 1Qt
  */
-export function abbreviate(n: number, threshold = 100_000, suffixes = baseSuffixes): string {
-	if (n < threshold)
-		return commaFormat(n);
+export function abbreviate(n: number, threshold = 1000, suffixes = defaultSuffixes): string {
+  if (n < threshold)
+    return commaFormat(n);
 
-	const index = floor(log(n, 1000)) - 1;
-	const divisor = 10 ** ((index + 1) * 3);
-	const [baseNumber] = "%.1f".format(floor(n / divisor)).gsub("%.?0+$", "");
-	return baseNumber + (index < 0 ? "" : suffixes[index]);
+  const index = floor(log(n, 1000)) - 1;
+  const divisor = 10 ** ((index + 1) * 3);
+  const [baseNumber] = "%.3f".format(n / divisor).gsub("%.?0+$", "");
+  return baseNumber + (index < 0 ? "" : suffixes[index]);
 }
 
 /**
  * Parses a number formatted by `abbreviate()` back into a number type
  * @example parseAbbreviatedNumber("1B") // 1000000000
  */
-export function parseAbbreviatedNumber(suffixed: string, suffixes = baseSuffixes): number {
-	const match = suffixed.gsub(",", "")[0].match("^([0-9,.]+)([KMBT]?)$");
-	if (!match)
-		return error("[@rbxts/formatting]: Invalid suffixed number format");
+export function parseAbbreviated(suffixed: string, suffixes = defaultSuffixes): number {
+  const regex = createCaseInsensitiveRegex(suffixes);
+  const match = trim(suffixed.gsub(",", "")[0]).match(`^([0-9,.]+)([(${regex})]?)$`);
+  if (!match)
+    throw "[@rbxts/formatting]: Invalid suffixed number format";
 
-	let numberPart = tostring(match[0]);
-	const suffix = tostring(match[1]);
+  let numberPart = tonumber(match[0])!;
+  const suffix = tostring(match[1]);
 
-	if (suffix && suffix !== "" && suffix !== "nil") {
-		const index = (<readonly string[]>suffixes).indexOf(suffix.lower());
-		if (index === -1)
-			return error("[@rbxts/formatting]: Invalid suffix in suffixed number");
+  if (suffix !== undefined && suffix !== "" && suffix !== "nil") {
+    const index = suffixes.indexOf(capitalize(suffix.lower()));
+    if (index === -1)
+      throw "[@rbxts/formatting]: Invalid suffix in suffixed number";
 
-		const multiplier = 10 ** ((index + 1) * 3);
-		numberPart = tostring(tonumber(numberPart)! * multiplier);
-	}
+    const multiplier = 10 ** ((index + 1) * 3);
+    numberPart *= multiplier;
+  }
 
-	return tonumber(numberPart)!;
+  return numberPart;
 }
 
 const s = 1, m = 60, h = 3600, d = 86400, w = 604800;
 const timePatterns = {
-	s, second: s, seconds: s,
-	m, minute: m, minutes: m,
-	h, hour: h, hours: h,
-	d, day: d, days: d,
-	w, week: w, weeks: w
+  s, second: s, seconds: s,
+  m, minute: m, minutes: m,
+  h, hour: h, hours: h,
+  d, day: d, days: d,
+  w, week: w, weeks: w
 };
 
 /**
@@ -80,50 +112,61 @@ const timePatterns = {
  * @example toSeconds("10m 20s") // 620
  */
 export function toSeconds(time: string): number {
-	let seconds = 0;
-	for (const [value, unit] of time.gsub(" ", "")[0].gmatch("(%d+)(%a)")) {
-		const timeUnit = <keyof typeof timePatterns>unit;
-		const figure = <number>value;
-		seconds += figure * timePatterns[timeUnit];
-	}
+  const timeNumber = tonumber(time);
+  if (timeNumber !== undefined)
+    return timeNumber;
 
-	return seconds;
+  let seconds = 0;
+  for (const [value, unit] of time.gsub(" ", "")[0].gmatch("(%d+)(%a)")) {
+    const timeUnit = <keyof typeof timePatterns>unit;
+    const figure = value as number;
+    seconds += figure * timePatterns[timeUnit];
+  }
+
+  return seconds;
 }
 
 /**
  * Takes a time in seconds and converts it to a remaining time string
  * @example toRemainingTime(310) // 5m 10s
  */
-export function toRemainingTime(seconds: number, secondsFormat = "%ds", minutesFormat = "%dm", hoursFormat = "%dh", daysFormat = "%dd"): string {
-	const days = floor(seconds / d);
-	seconds %= d;
+export function toRemainingTime(
+  seconds: number,
+  secondsFormat = "%ds",
+  minutesFormat = "%dm",
+  hoursFormat = "%dh",
+  daysFormat = "%dd"
+): string {
+  const days = floor(seconds / d);
+  seconds %= d;
 
-	const hours = floor(seconds / h);
-	seconds %= h;
+  const hours = floor(seconds / h);
+  seconds %= h;
 
-	const minutes = floor(seconds / m);
-	seconds %= m;
+  const minutes = floor(seconds / m);
+  seconds %= m;
 
-	let remainingTime = "";
-	if (days > 0)
-		remainingTime += (daysFormat + " ").format(days);
-	if (hours > 0)
-		remainingTime += (hoursFormat + " ").format(hours);
-	if (minutes > 0)
-		remainingTime += (minutesFormat + " ").format(minutes);
-	if (seconds > 0)
-		remainingTime += (secondsFormat + " ").format(seconds);
+  let remainingTime = "";
+  if (days > 0)
+    remainingTime += (daysFormat + " ").format(days);
+  if (hours > 0)
+    remainingTime += (hoursFormat + " ").format(hours);
+  if (minutes > 0)
+    remainingTime += (minutesFormat + " ").format(minutes);
+  if (seconds > 0)
+    remainingTime += (secondsFormat + " ").format(seconds);
 
-	return trim(remainingTime);
+  return trim(remainingTime);
 }
 
 /**
- * Takes a time in seconds and converts it to a long remaining time string
+ * Takes a time in seconds and converts it to a long remaining time string (HH:MM:SS)
  * @example toLongRemainingTime(3690) // 01:01:30
  */
 export function toLongRemainingTime(seconds: number): string {
-	const hours = floor(seconds / h);
-	const minutes = floor((seconds % h) / m);
-	const remainingSeconds = seconds % m;
-	return "%02d:%02d:%02d".format(hours, minutes, remainingSeconds);
+  const hours = floor(seconds / h);
+  const minutes = floor((seconds % h) / m);
+  const remainingSeconds = seconds % m;
+
+  return "%02d:%02d:%02d".format(hours, minutes, remainingSeconds);
 }
---
+++ tests/src/spec.ts
@@ -0,0 +1,76 @@
+import { Assert, Fact, Theory, InlineData } from "@rbxts/runit";
+import { commaFormat, abbreviate, parseAbbreviated, toSeconds, toRemainingTime, toLongRemainingTime } from "../../src";
+
+class FormattingTest {
+  @Theory
+  @InlineData(1_000_000, "1,000,000")
+  @InlineData(1_000_000.123, "1,000,000.123")
+  @InlineData(100_000, "100,000", 100_000)
+  @InlineData(99_999, "99999", 100_000)
+  public commaFormat(input: number, expected: string, minimum?: number): void {
+    Assert.equal(expected, commaFormat(input, minimum));
+  }
+
+  @Theory
+  @InlineData(1_000, "1K")
+  @InlineData(100_000, "100K")
+  @InlineData(1_000_000, "1M")
+  @InlineData(1_500_000, "1.5M")
+  @InlineData(1_560_000, "1.56M")
+  @InlineData(1_567_000, "1.567M")
+  @InlineData(1_568_000, "1.568M")
+  @InlineData(1_000_000_000, "1B")
+  @InlineData(1_000_000_000_000_000_000, "1Qt")
+  public abbreviate(input: number, expected: string, minimum?: number): void {
+    Assert.equal(expected, abbreviate(input, minimum));
+  }
+
+  @Theory
+  @InlineData("1K", 1_000)
+  @InlineData("100k", 100_000)
+  @InlineData("1M", 1_000_000)
+  @InlineData("1.5M", 1_500_000)
+  @InlineData("1.56M", 1_560_000)
+  @InlineData("1.567M", 1_567_000)
+  @InlineData("1.568M", 1_568_000)
+  @InlineData("1b", 1_000_000_000)
+  public parseAbbreviated(input: string, expected: number): void {
+    Assert.equal(expected, parseAbbreviated(input));
+  }
+
+  @Theory
+  @InlineData("30", 30)
+  @InlineData("30s", 30)
+  @InlineData("30 seconds", 30)
+  @InlineData("30 secs", 30)
+  @InlineData("30 sec", 30)
+  @InlineData("1m", 60)
+  @InlineData("2 mins", 120)
+  @InlineData("2 minutes", 120)
+  @InlineData("2 minutes 30s", 150)
+  @InlineData("2m 30s", 150)
+  @InlineData("1d 2h 3m 2s", 93_782)
+  public toSeconds(input: string, expected: number): void {
+    Assert.equal(expected, toSeconds(input));
+  }
+
+  @Theory
+  @InlineData(30, "30s")
+  @InlineData(180, "3m")
+  @InlineData(310, "5m 10s")
+  @InlineData(3910, "1h 5m 10s")
+  public toRemainingTime(input: number, expected: string): void {
+    Assert.equal(expected, toRemainingTime(input));
+  }
+
+  @Theory
+  @InlineData(30, "00:00:30")
+  @InlineData(180, "00:03:00")
+  @InlineData(310, "00:05:10")
+  @InlineData(3910, "01:05:10")
+  public toLongRemainingTime(input: number, expected: string): void {
+    Assert.equal(expected, toLongRemainingTime(input));
+  }
+}
+
+export = FormattingTest;
